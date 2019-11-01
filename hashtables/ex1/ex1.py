23c3ce9cc9ba7b2e89d138a6f408a14c316e6dca
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-10-31 21:33:31 -0500

    done, but no coins mined

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,17 +5,28 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
+    # hashtable capacity is 16
     ht = HashTable(16)
 
     """
     YOUR CODE HERE
     """
+    # loop through the length of the weights array
     for i in range(len(weights)):
+
+        # get the value from key at i  
+        # from the hash table, subtract the limit from value of weight[i]
         wght = weights[i]
         idx = hash_table_retrieve(ht, limit - wght)
+        
+        # if limit - value at wght[i] is true, push the value of wght[i] and the index
+        # at which the makes the value true, into a tuple, return the tuple.
         if idx != None:
             answer = (i, idx)
+           
             return answer
+
+        # insert hash table position of value of weight[i] and i
         hash_table_insert(ht, wght, i)
 
     return None
